22ccbddedb83ec6fdbfb93994f24d4946d1e503f
Jeff Terstriep <jefft@illinois.edu>
2017-08-18 15:45:23 -0500

    Use terrautils branch for testing.

--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+        'numpy',
         'wtforms',
         'requests',
         'Pillow',
+        'terrautils'
+    ],
+    dependency_links=[
+        'git+https://github.com/terraref/terrautils@standard_sensor_path#egg=terrautils-1.0.0'
     ],
 )
